79a2351813fec0090f8f9bb9806a0624ae7f7df7
Vassili Gorshkov <vassili.gorshkov@sezzle.com>
2024-10-28 19:56:46 -0700

    Make processor a constant label for batch prometheus metrics.

--- batcher/prometheus.go
+++ batcher/prometheus.go
@@ -10,47 +10,49 @@ type PrometheusMetricsCollector struct {
 	itemsProcessed     *prometheus.CounterVec
 	processingDuration *prometheus.HistogramVec
 	errors             *prometheus.CounterVec
-	processorName      string
 }
 
 func NewPrometheusMetricsCollector(processorName string) *PrometheusMetricsCollector {
 	return &PrometheusMetricsCollector{
 		batchesProcessed: metrics.SafeNewCounterVec(
 			prometheus.CounterOpts{
-				Name: "batcher_batches_processed_total",
-				Help: "The total number of batches processed",
+				Name:        "batcher_batches_processed_total",
+				Help:        "The total number of batches processed",
+				ConstLabels: prometheus.Labels{"processor": processorName},
 			},
-			[]string{"processor"},
+			[]string{},
 		),
 		itemsProcessed: metrics.SafeNewCounterVec(
 			prometheus.CounterOpts{
-				Name: "batcher_items_processed_total",
-				Help: "The total number of items processed",
+				Name:        "batcher_items_processed_total",
+				Help:        "The total number of items processed",
+				ConstLabels: prometheus.Labels{"processor": processorName},
 			},
-			[]string{"processor"},
+			[]string{},
 		),
 		processingDuration: metrics.SafeNewHistogramVec(
 			prometheus.HistogramOpts{
-				Name:    "batcher_processing_duration_seconds",
-				Help:    "The duration of batch processing operations",
-				Buckets: prometheus.ExponentialBuckets(0.001, 2, 10),
+				Name:        "batcher_processing_duration_seconds",
+				Help:        "The duration of batch processing operations",
+				Buckets:     prometheus.ExponentialBuckets(0.001, 2, 10),
+				ConstLabels: prometheus.Labels{"processor": processorName},
 			},
-			[]string{"processor"},
+			[]string{},
 		),
 		errors: metrics.SafeNewCounterVec(
 			prometheus.CounterOpts{
-				Name: "batcher_errors_total",
-				Help: "The total number of errors encountered during batch processing",
+				Name:        "batcher_errors_total",
+				Help:        "The total number of errors encountered during batch processing",
+				ConstLabels: prometheus.Labels{"processor": processorName},
 			},
-			[]string{"processor"},
+			[]string{},
 		),
-		processorName: processorName,
 	}
 }
 
 func (p *PrometheusMetricsCollector) Collect(metrics BatchMetrics) {
-	p.batchesProcessed.WithLabelValues(p.processorName).Add(float64(metrics.BatchesProcessed))
-	p.itemsProcessed.WithLabelValues(p.processorName).Add(float64(metrics.ItemsProcessed))
-	p.processingDuration.WithLabelValues(p.processorName).Observe(metrics.TotalProcessingTime.Seconds())
-	p.errors.WithLabelValues(p.processorName).Add(float64(metrics.Errors))
+	p.batchesProcessed.WithLabelValues().Add(float64(metrics.BatchesProcessed))
+	p.itemsProcessed.WithLabelValues().Add(float64(metrics.ItemsProcessed))
+	p.processingDuration.WithLabelValues().Observe(metrics.TotalProcessingTime.Seconds())
+	p.errors.WithLabelValues().Add(float64(metrics.Errors))
 }
