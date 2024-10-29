770e8969e84d24a44b805529099e303d4790f6f6
Vassili Gorshkov <vassili.gorshkov@sezzle.com>
2024-10-28 19:49:51 -0700

    Removed unused batcher prometheus Setup() function

--- batcher/prometheus.go
+++ batcher/prometheus.go
@@ -47,40 +47,6 @@ func NewPrometheusMetricsCollector(processorName string) *PrometheusMetricsColle
 		processorName: processorName,
 	}
 }
-func (p *PrometheusMetricsCollector) Setup(batcher *BatchProcessor[any]) {
-	p.batchesProcessed = metrics.SafeNewCounterVec(
-		prometheus.CounterOpts{
-			Name: "batcher_batches_processed_total",
-			Help: "The total number of batches processed",
-		},
-		[]string{"processor"},
-	)
-
-	p.itemsProcessed = metrics.SafeNewCounterVec(
-		prometheus.CounterOpts{
-			Name: "batcher_items_processed_total",
-			Help: "The total number of items processed",
-		},
-		[]string{"processor"},
-	)
-
-	p.processingDuration = metrics.SafeNewHistogramVec(
-		prometheus.HistogramOpts{
-			Name:    "batcher_processing_duration_seconds",
-			Help:    "The duration of batch processing operations",
-			Buckets: prometheus.ExponentialBuckets(0.001, 2, 10),
-		},
-		[]string{"processor"},
-	)
-
-	p.errors = metrics.SafeNewCounterVec(
-		prometheus.CounterOpts{
-			Name: "batcher_errors_total",
-			Help: "The total number of errors encountered during batch processing",
-		},
-		[]string{"processor"},
-	)
-}
 
 func (p *PrometheusMetricsCollector) Collect(metrics BatchMetrics) {
 	p.batchesProcessed.WithLabelValues(p.processorName).Add(float64(metrics.BatchesProcessed))
